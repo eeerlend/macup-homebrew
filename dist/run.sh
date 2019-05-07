7ed7ea207e78d4796ed7b7d8a8d7c53c00143e41
Erlend Mongstad <eeerlend@gmail.com>
2019-05-07 20:37:47 +0200

    style: created function for brew install/upgrade

--- dist/run.sh
+++ dist/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function install_or_upgrade_brew_package {
+  local package=$1
+	if brew ls --versions "$package" >/dev/null; then
+		if (brew outdated | grep "$package" > /dev/null); then 
+      brew upgrade "$package"; 
+    else 
+      report_from_package " $package is already up to date"; 
+    fi
+  else
+    report_from_package "$package being installed"
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install "$package"
+  fi
+}
+
 # Check for Homebrew, install if we don't have it
 if [ ! $(which brew) ]; then
 	report_from_package "Homebrew is not installed, proceed with installation."
@@ -17,14 +31,5 @@ brew analytics off
 # todo: check if array is declared up front!
 # shellcheck disable=SC2154
 for ((i=0; i<${#macup_homebrew_packages[@]}; ++i)); do
-	if brew ls --versions "${macup_homebrew_packages[i]}" >/dev/null; then
-		if (brew outdated | grep "${macup_homebrew_packages[i]}" > /dev/null); then 
-      brew upgrade "${macup_homebrew_packages[i]}"; 
-    else 
-      report_from_package " ${MACUP_HOMEBREW_PACKAGES[i]} is already up to date"; 
-    fi
-  else
-    report_from_package "${macup_homebrew_packages[i]} being installed"
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install "${macup_homebrew_packages[i]}"
-  fi
+  install_or_upgrade_brew_package "${macup_homebrew_packages[i]}"
 done
